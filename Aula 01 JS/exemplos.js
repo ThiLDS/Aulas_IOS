fbcb0d71ac19a4a7413010ce94ad6ff9e76f6586
Thiago <thi_lopes01@hotmail.com>
2024-10-07 11:50:19 -0300

    Salvando aulas JS

---
+++ Aula 01 JS/exemplos.js
@@ -0,0 +1,31 @@
+//let nome = `Thiago`
+//let idade = 23
+//let matriculaativa = true
+
+//console.log(typeof nome, nome);
+//console.log(typeof idade, idade);
+//console.log(typeof matriculaativa, matriculaativa);
+
+
+
+
+
+//let nome = 'Thiago'
+//let sNome = "Lopes"
+//let solteiro = `Não`
+//let idade = 23
+
+//console.log(nome + " " + sNome + "Está Solteiro? " + solteiro);
+//console.log(nome, sNome, "Está solteiro", solteiro, "sua idade é: ", idade);
+//console.log(`Olá, ${nome}! Você está solteiro? ${solteiro}, sua idade é: ${idade = 25*2}`);
+
+
+
+//let nota01, nota02, media;
+
+//nota01 = 10;
+//nota02 = 04;
+//media = (nota01 + nota02) / 2
+//console.log();
+
+ // Média Apr = 7
---
+++ Aula 01 JS/main.js
@@ -0,0 +1,12 @@
+let nome = 'Thiago'
+let nota01 = 5 * 0.6;
+let nota02 = 10 * 0.4;
+let media = (nota01  + nota02 )
+ 
+
+
+console.log("O aluno",nome,"com a média",media, "está aprovado!");
+console.log(nota01);
+console.log(nota02);
+console.log(media);
+console.log();
